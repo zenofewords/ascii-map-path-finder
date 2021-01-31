e4bf2a9cbc41def04c73e2ec8c7056831d78e672
Dominik Zen <zen.dominik@gmail.com>
2021-01-31 13:01:53 +0100

    Use uppercase for constants, refactor

--- ascii_map_path_finder.py
+++ ascii_map_path_finder.py
@@ -2,17 +2,15 @@ import argparse
 import re
 
 from constants import (
-    regex_lookup,
-    start_symbol,
-    end_symbol,
-    intersection_symbol,
-    up,
-    right,
-    down,
-    left,
+    REGEX_LOOKUP,
+    START_SYMBOL,
+    END_SYMBOL,
+    CORNER_SYMBOL,
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT,
 )
-parser = argparse.ArgumentParser(description='Find travel path in arbitrary ASCII map.')
-parser.add_argument('ascii_maps', type=str, nargs='+', help='a path to an ASCII map text file')
 
 
 def parse_map(ascii_map_path):
@@ -35,13 +33,13 @@ def create_map(lines):
 
 
 def parse_char(char):
-    match = re.search(regex_lookup, char)
+    match = re.search(REGEX_LOOKUP, char)
     return match.string if match else None
 
 
 def validate_map(ascii_map):
-    verify_char_count(ascii_map, start_symbol, 1)
-    verify_char_count(ascii_map, end_symbol, 1)
+    verify_char_count(ascii_map, START_SYMBOL, 1)
+    verify_char_count(ascii_map, END_SYMBOL, 1)
 
 
 def verify_char_count(ascii_map, char, count):
@@ -60,7 +58,7 @@ def travel_map(ascii_map):
         update_letters(symbol, letters, x, y)
         travel_path.append(symbol)
 
-        if symbol == end_symbol:
+        if symbol == END_SYMBOL:
             break
         x, y, orientation = find_next_symbol(ascii_map, x, y, orientation)
 
@@ -70,7 +68,7 @@ def travel_map(ascii_map):
 def find_beginning(ascii_map):
     for x, column in enumerate(ascii_map):
         for y, row in enumerate(column):
-            if row and start_symbol in row:
+            if row and START_SYMBOL in row:
                 return x, y
 
 
@@ -84,7 +82,7 @@ def update_letters(symbol, letters, x, y):
 
 
 def find_next_symbol(ascii_map, x, y, orientation):
-    if ascii_map[x][y] in [start_symbol, intersection_symbol]:
+    if ascii_map[x][y] in [START_SYMBOL, CORNER_SYMBOL]:
         return find_direction(ascii_map, x, y, orientation)
 
     try:
@@ -98,15 +96,15 @@ def find_next_symbol(ascii_map, x, y, orientation):
 
 def find_direction(ascii_map, x, y, orientation):
     x_spread = [x]
-    if x > 0 and orientation != down:
+    if x > 0 and orientation != DOWN:
         x_spread.append(x - 1)
-    if orientation != up:
+    if orientation != UP:
         x_spread.append(x + 1)
 
     y_spread = [y]
-    if y > 0 and orientation != right:
+    if y > 0 and orientation != RIGHT:
         y_spread.append(y - 1)
-    if orientation != left:
+    if orientation != LEFT:
         y_spread.append(y + 1)
 
     coordinates = [(a, b) for a in x_spread for b in y_spread]
@@ -121,10 +119,10 @@ def find_direction(ascii_map, x, y, orientation):
 
 
 def follow_orientation(x, y, orientation):
-    x = x + 1 if orientation == down else x
-    x = x - 1 if orientation == up else x
-    y = y + 1 if orientation == right else y
-    y = y - 1 if orientation == left else y
+    x = x + 1 if orientation == DOWN else x
+    x = x - 1 if orientation == UP else x
+    y = y + 1 if orientation == RIGHT else y
+    y = y - 1 if orientation == LEFT else y
     return x, y, orientation
 
 
@@ -132,9 +130,9 @@ def find_orientation(x, y, new_x, new_y):
     orientation = None
 
     if x != new_x:
-        orientation = down if x < new_x else up
+        orientation = DOWN if x < new_x else UP
     if y != new_y:
-        orientation = right if y < new_y else left
+        orientation = RIGHT if y < new_y else LEFT
     return new_x, new_y, orientation
 
 
@@ -143,16 +141,21 @@ def print_results(letters, travel_path):
     print(''.join(travel_path))
 
 
-def run_path_finder(args):
+def run_path_finder(ascii_map_path):
+    try:
+        ascii_map = parse_map(ascii_map_path)
+        validate_map(ascii_map)
+        print_results(*travel_map(ascii_map))
+    except FileNotFoundError:
+        print('File does not exist: {}'.format(ascii_map_path))
+    except ValueError as error_message:
+        print('{}: {}'.format(error_message, ascii_map_path))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Find travel path in arbitrary ASCII map.')
+    parser.add_argument('ascii_maps', type=str, nargs='+', help='a path to an ASCII map text file')
+    args = parser.parse_args()
+
     for ascii_map_path in args.ascii_maps:
-        try:
-            ascii_map = parse_map(ascii_map_path)
-            validate_map(ascii_map)
-            print_results(*travel_map(ascii_map))
-        except FileNotFoundError:
-            print('File does not exist: {}'.format(ascii_map_path))
-        except ValueError as error_message:
-            print('{}: {}'.format(error_message, ascii_map_path))
-
-
-run_path_finder(parser.parse_args())
+        run_path_finder(ascii_map_path)
